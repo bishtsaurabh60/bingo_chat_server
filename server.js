9d713cf9c086efeb4d1785bc0e9e061610f72061
bishtsaurabh60 <75714311+bishtsaurabh60@users.noreply.github.com>
2022-12-20 12:54:26 +0530

    redirection fixes

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use('/api/message', messageRoutes);
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname,'..',"build", "index.html"));
+  res.sendFile(path.join(__dirname,'..',"client","build", "index.html"));
 });
 
 // app.all("*", (req, res) => {
